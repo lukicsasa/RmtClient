903ea27df817b8f937933757b9d6d3d5c460f41e
lukicsasa <lukic.sasa11@gmail.com>
2015-12-23 01:16:40 +0100

    sredjen malo kod

--- src/Client.java
+++ src/Client.java
@@ -2,10 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
-import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
-
 
 public class Client {
 
@@ -18,65 +15,65 @@ public class Client {
 	static PrintStream dataOutputStream = null;
 	static int controlPort = 1908;
 	static int dataPort = 20513;
-	
+
 	static boolean start = true;
 	static boolean flag = true;
-	
+
 	public static void main(String[] args) {
 
 		try {
-			controlSocket = new Socket("localhost",controlPort);
-			controlInputStream = new BufferedReader(new InputStreamReader(controlSocket.getInputStream()));
-			controlOutputStream = new PrintStream(controlSocket.getOutputStream());
+			controlSocket = new Socket("localhost", controlPort);
+			controlInputStream = new BufferedReader(new InputStreamReader(
+					controlSocket.getInputStream()));
+			controlOutputStream = new PrintStream(
+					controlSocket.getOutputStream());
 			console = new BufferedReader(new InputStreamReader(System.in));
-			
-			while(flag){
-				if(start){
+
+			while (flag) {
+				if (start) {
 					System.out.println(controlInputStream.readLine());
 					System.out.println(controlInputStream.readLine());
 					start = false;
 				}
 
-				while(true){
-				System.out.println(controlInputStream.readLine());
-				controlOutputStream.println(console.readLine());
-				String answer = controlInputStream.readLine();
-				System.out.println(answer);
-				if(answer.equals("Goodbye!"))
-					return;
-				if(!answer.startsWith("Invalid")) {
-					break;
+				while (true) {
+					System.out.println(controlInputStream.readLine());
+					controlOutputStream.println(console.readLine());
+					String answer = controlInputStream.readLine();
+					System.out.println(answer);
+					if (answer.equals("Goodbye!"))
+						return;
+					if (!answer.startsWith("Invalid")) {
+						break;
+					}
 				}
-				}
-				
+
 				System.out.println(controlInputStream.readLine());
-				
-				//podaci
-				dataSocket = new Socket("localhost",dataPort);
-				dataInputStream = new BufferedReader(new InputStreamReader(dataSocket.getInputStream()));
-				dataOutputStream = new  PrintStream(dataSocket.getOutputStream());
-				
-				while(true){
-				dataOutputStream.println(console.readLine());
-				String numAnswer = dataInputStream.readLine();
-				System.out.println(numAnswer);
-				if(numAnswer.equals("Goodbye!"))
-					return;
-				if(!numAnswer.startsWith("Please type in ONLY") && !numAnswer.startsWith("You can't divide by zero. Please type"))
-					break;
+
+				dataSocket = new Socket("localhost", dataPort);
+				dataInputStream = new BufferedReader(new InputStreamReader(
+						dataSocket.getInputStream()));
+				dataOutputStream = new PrintStream(dataSocket.getOutputStream());
+
+				while (true) {
+					dataOutputStream.println(console.readLine());
+					String numAnswer = dataInputStream.readLine();
+					System.out.println(numAnswer);
+					if (numAnswer.equals("Goodbye!"))
+						return;
+					if (!numAnswer.startsWith("Please type in ONLY")
+							&& !numAnswer
+									.startsWith("You can't divide by zero. Please type"))
+						break;
 				}
 				dataSocket.close();
 			}
-			
-			
+
 			controlSocket.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		
+
 	}
 
 }
